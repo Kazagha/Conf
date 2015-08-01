4de0dd1832df6f9b2835ddfece940929b3bbba08
Kazagha <chrisg.local@gmail.com>
2015-08-01 15:42:24 +1000

    ConfNode: Add setter methods for the ConfData in the child array.

--- src/net/arcanesanctuary/Configuration/ConfNode.java
+++ src/net/arcanesanctuary/Configuration/ConfNode.java
@@ -32,6 +32,28 @@ public class ConfNode extends DefaultMutableTreeNode {
 		return null;
 	}
 	
+	public void set(String variable, String value) {
+		set(variable, null, value);
+	}
+	
+	public void set(String variable, String desc, String value) {
+		int index = this.getIndexOf(variable);
+		
+		if(index != -1) {	
+			ConfData cd = this.getConfAt(index);
+			
+			if(desc != null) 
+				this.getConfAt(index).setVal(desc);
+						
+			if(value != null) 
+				this.getConfAt(index).setVal(value);
+				
+		} else {
+			this.add(new ConfNode(new ConfData(variable, desc, value)));
+		}
+		
+	}
+	
 	public int getIndexOf(String variable) {
 		for(int i = 0; i < this.getChildCount() - 1; i++) {
 			ConfData cd = this.getConfAt(i);
