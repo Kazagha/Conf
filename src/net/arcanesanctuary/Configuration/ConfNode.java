5371951b80b03aed8004669828c101102ff013b8
Kazagha <chrisg.local@gmail.com>
2015-08-01 15:40:23 +1000

    ConfNode: Add getter method. This method gets the vaule of the variable underneath this.

--- src/net/arcanesanctuary/Configuration/ConfNode.java
+++ src/net/arcanesanctuary/Configuration/ConfNode.java
@@ -11,4 +11,24 @@ public class ConfNode extends DefaultMutableTreeNode {
 	public ConfNode(String var, String desc, String val) {
 		this.userObject = new ConfData(var, desc, val);
 	}
+	
+	public ConfData getConfAt(int i) {
+		return (ConfData) (((ConfNode) this.getChildAt(i)).getUserObject());
+	}
+	
+	public ConfData getConfData() {
+		return (ConfData) this.userObject;
+	}
+	
+	public String get(String variable) {
+		for(int i = 0; i < this.getChildCount() - 1; i++) {
+			ConfData cd = this.getConfAt(i);
+			
+			if(cd.getVar().equals(variable)) {
+				return cd.getVal();
+			}
+		}
+		
+		return null;
+	}		
 }
