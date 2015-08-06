6f762bae8377ab84532ca248e5304435e9aa146c
Kazagha <chrisg.local@gmail.com>
2015-08-06 19:12:04 +1000

    XMLController: Initial commit.

---
+++ src/net/arcanesanctuary/Configuration/XMLController.java
@@ -0,0 +1,20 @@
+package net.arcanesanctuary.Configuration;
+
+import java.io.File;
+
+public class XMLController {
+	
+	File filename;
+
+	public XMLController(File f) {
+		filename = f;
+	}
+	
+	public void save(ConfData cd) {
+		
+	}
+	
+	public void load(ConfData cd) {
+		
+	}
+}
