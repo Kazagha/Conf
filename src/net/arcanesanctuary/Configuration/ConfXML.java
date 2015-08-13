ed94bb985c0396257871f0fd2b0a764bbebb5050
Kazagha <chrisg.local@gmail.com>
2015-08-13 17:58:53 +1000

    ConfXML: Initial commit.

---
+++ src/net/arcanesanctuary/Configuration/ConfXML.java
@@ -0,0 +1,62 @@
+package net.arcanesanctuary.Configuration;
+
+import java.util.ArrayList;
+import java.util.List; 
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+@XmlRootElement
+@XmlType(propOrder={ "name", "desc", "value" })
+public class ConfXML {
+
+	private String name;
+	private String value;
+	private String description;
+	private ArrayList<ConfXML> childNodes;
+	
+	public ConfXML() {
+		this.childNodes = new ArrayList<ConfXML>();
+	}
+	
+	public ConfXML(String name, String val, String desc) {
+		this.name = name;
+		this.value = val;
+		this.description = desc;
+		this.childNodes = new ArrayList<ConfXML>();
+	}
+	
+	public void setName(String str) {
+		this.name = str;
+	}
+	
+	public void setValue(String str) {
+		this.value = str;
+	}
+	
+	public void setDesc(String str) {
+		this.description = str;
+	}
+	
+	public void setChildNodes() {
+		
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}	
+	
+	public String getDesc() {
+		return this.description;
+	}
+		
+	public ArrayList<ConfXML> getChildNodes() {
+		return this.childNodes;
+	}
+}
