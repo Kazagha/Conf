f91b57ebdfe7f03e9224c30e25c307e37f4e4633
Kazagha <chrisg.local@gmail.com>
2015-08-24 15:16:17 +1000

    XMLController: Create the 'load' method

--- src/net/arcanesanctuary/Configuration/XMLController.java
+++ src/net/arcanesanctuary/Configuration/XMLController.java
@@ -3,6 +3,10 @@ package net.arcanesanctuary.Configuration;
 import java.io.File;
 import java.io.IOException;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.Marshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -14,30 +18,44 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
-
 public class XMLController {
 	
 	File fileName;
-	Document doc;
+	
+	private JAXBContext jaxbContext; 
+	private Unmarshaller unmarshaller = null;
+	private Marshaller marshaller = null;
 
 	public XMLController(File file) {
-		this.setFileName(file);
+		this.fileName = file;
+		this.setUp();
 	}
 	
-	public void setFileName(File file) {
-		if(! fileName.exists()) {
-			try {
-				fileName.createNewFile();
-			} catch (IOException e) {
-				System.out.format("Cannot create file in the following location%n%s%n",
-						fileName.toString());
-				e.printStackTrace();
-			}
+	private void setUp() {
+		try {
+			jaxbContext = JAXBContext.newInstance(Conf.class);		
+			unmarshaller = jaxbContext.createUnmarshaller();
+			marshaller = jaxbContext.createMarshaller();
+		} catch (JAXBException e) {
+			System.out.format("Error configuring JAXB / Marshaller%n");
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean checkFileName(File file) {
+		if(fileName.exists()) {
+			return true;
 		}
 		
-		this.fileName = file;
+		try {
+			fileName.createNewFile();
+		} catch (IOException e) {
+			System.out.format("Cannot create file in the following location%n%s%n",
+					fileName.toString());
+			e.printStackTrace();
+		}
+		
+		return false;
 	}
 	
 	public void save(Conf conf) {
@@ -45,7 +63,15 @@ public class XMLController {
 	}
 	
 	public Conf load() {
-		return new Conf();
-	}
-	
+		if(checkFileName(fileName)) {
+			try {
+				return (Conf) unmarshaller.unmarshal(fileName);
+			} catch (JAXBException e) {
+				System.out.format("Error loading file: %s", fileName.toString());
+				e.printStackTrace();
+			}
+		}
+		
+		return new Conf("root", null, null);
+	}	
 }
